6ee028651cfd9c0b0f2c2291a182dbce61923d2e
ATGsan <alex.shitov1237@gmail.com>
2021-05-01 20:52:21 +0300

    Added simple OPT algorithm

--- client/client.cc
+++ client/client.cc
@@ -17,13 +17,18 @@ using operationTransportation::editor_request;
 using operationTransportation::empty;
 using operationTransportation::file_from_server;
 using operationTransportation::OP_type;
+using operationTransportation::last_executed_operation;
 
 // main class for client
 class ClientService {
+private:
+    std::unique_ptr<clientService::Stub> stub_;
+    std::string file_name;
+    uint64_t last_op;
 public:
     // default constructor with initialize of channel between server and client
     ClientService(std::shared_ptr<Channel> channel, std::string& file_path) :
-                  stub_(clientService::NewStub(channel)), file_name(file_path) {}
+            stub_(clientService::NewStub(channel)), file_name(file_path) {}
 
     // command to initialize file in client side
     void initialize() {
@@ -31,43 +36,43 @@ public:
         file_from_server reply;
         ClientContext context;
         // call rpc in server side
-        Status status = stub_->sendFile(&context, e, &reply);
+        Status status = stub_->initialize(&context, e, &reply);
         std::fstream file(file_name);
         for(int i = 0; i < reply.file_size(); i++) {
-            file << reply.file(i);
-            file << '\n';
+            file << reply.file(i) << std::endl;
         }
+        last_op = reply.op_id();
     }
 
     // main method to send operation to server
     void OPs(OP_type operation, uint64_t pos, uint64_t line, char sym, uint32_t user_id) {
-        empty e;
+        last_executed_operation op;
         editor_request request;
         request.set_op(operation);
         request.set_pos(pos);
         request.set_line(line);
         request.set_sym(sym);
         request.set_user_id(user_id);
+        request.set_op_id(last_op);
         ClientContext context;
-        Status status = stub_->sendOP(&context, request, &e);
+        Status status = stub_->sendOP(&context, request, &op);
+        last_op = op.op_id();
     }
 
     // call to write file from vector of strings to file in server file
     void writeToFile() {
         empty e;
+        last_executed_operation op;
         ClientContext context;
-        Status status = stub_->writeToFile(&context, e, &e);
+        Status status = stub_->writeToFile(&context, e, &op);
+        last_op = op.op_id();
     }
-private:
-    std::unique_ptr<clientService::Stub> stub_;
-    std::string file_name;
 };
 
 void RunClient(std::string& file, std::string& server_address) {
     ClientService client(
-        grpc::CreateChannel(server_address,
-        grpc::InsecureChannelCredentials()), file);
-
+            grpc::CreateChannel(server_address,
+                                grpc::InsecureChannelCredentials()), file);
 }
 
 int main(int argc, char* argv[]) {
--- server/server.cc
+++ server/server.cc
@@ -23,6 +23,7 @@ using operationTransportation::editor_request;
 using operationTransportation::file_from_server;
 using operationTransportation::empty;
 using operationTransportation::OP_type;
+using operationTransportation::last_executed_operation;
 
 bool file_exist(const std::string& file_path) {
     if(fopen(file_path.c_str(), "r+")) {
@@ -44,16 +45,13 @@ public:
     size_t get_size() const {
         return size;
     }
-
     size_t get_pos() const {
         return pos;
     }
-
     size_t set_size(size_t arg) {
         size = arg;
         return size;
     }
-
     size_t set_pos(size_t arg) {
         pos = arg;
         return pos;
@@ -109,28 +107,73 @@ public:
         auto a = *(executed_operations.begin() + pos++);
         return a;
     }
+
+    void OPT(editor_request& request) {
+        OP_type op_type = request.op();
+        if(op_type == operationTransportation::UNDO || op_type == operationTransportation::REDO) {
+            return;
+        }
+        uint64_t l = request.line();
+        uint64_t p = request.pos();
+        if(pos == 0) {
+            return;
+        }
+        size_t i = pos - 1;
+        editor_request tmp = executed_operations[i];
+        while(request.op_id() != tmp.op_id() && i != 0) {
+            switch (tmp.op()) {
+                case operationTransportation::INSERT: {
+                    if(tmp.line() == l && tmp.pos() < p) {
+                        ++p;
+                    }
+                    break;
+                }
+                case operationTransportation::DELETE: {
+                    if(tmp.line() == l && tmp.pos() < p) {
+                        --p;
+                    }
+                    break;
+                }
+                case operationTransportation::ADD_LINE: {
+                    if(tmp.line() < l) {
+                        ++l;
+                    } else if(tmp.line() == l && tmp.pos() < p) {
+                        p = p - tmp.pos();
+                    }
+                    break;
+                }
+                case operationTransportation::DEL_LINE: {
+                    if(tmp.line() <= l) {
+                        --l;
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+            tmp = executed_operations[--i];
+        }
+        request.set_pos(p);
+        request.set_line(l);
+    }
 };
 
 void insert(std::vector<std::string>& content, char sym, uint64_t pos, uint64_t line) {
     content[line].insert(pos, 1, sym);
 }
-
 void del(std::vector<std::string>& content, char sym, uint64_t pos, uint64_t line) {
     content[line].erase(pos, 1);
 }
-
 void add_line(std::vector<std::string>& content, uint64_t pos, uint64_t line) {
     std::string str = content[line].substr(pos, content[line].size() - pos);
     content.insert(content.begin() + line + 1, "");
     content[line + 1] = str;
     content[line].erase(content[line].begin() + pos, content[line].end());
 }
-
 void del_line(std::vector<std::string>& content, uint64_t line) {
     content[line - 1].append(content[line]);
     content.erase(content.begin() + line);
 }
-
 void undo(EOpVector& exec_operations, std::vector<std::string>& content) {
     auto a = exec_operations.get_for_undo();
     OP_type op_type = a.op();
@@ -138,6 +181,7 @@ void undo(EOpVector& exec_operations, std::vector<std::string>& content) {
     uint64_t line = a.line();
     char sym = a.sym();
     uint32_t user_id = a.user_id();
+    uint64_t last_op = a.op_id();
     switch (op_type) {
         case operationTransportation::INSERT: {
             insert(content, sym, pos, line);
@@ -159,7 +203,6 @@ void undo(EOpVector& exec_operations, std::vector<std::string>& content) {
             return;
     }
 }
-
 void redo(EOpVector& exec_operations, std::vector<std::string>& content) {
     auto a = exec_operations.get_for_redo();
     OP_type op_type = a.op();
@@ -167,6 +210,7 @@ void redo(EOpVector& exec_operations, std::vector<std::string>& content) {
     uint64_t line = a.line();
     char sym = a.sym();
     uint32_t user_id = a.user_id();
+    uint64_t last_op = a.op_id();
     switch (op_type) {
         case operationTransportation::INSERT: {
             insert(content, sym, pos, line);
@@ -195,6 +239,7 @@ private:
     std::string file_name;
     std::vector<std::string> content;
     EOpVector op_vector;
+    uint64_t op_quantity;
 public:
     ServerService(std::string &file) : file_name(std::move(file)) {
         std::ifstream f(file_name);
@@ -202,8 +247,8 @@ public:
         while (std::getline(f, s)) {
             content.push_back(s);
         }
+        op_quantity = 0;
     }
-
     ~ServerService() {
         std::ofstream f(file_name);
         for(std::string& s : content) {
@@ -211,37 +256,41 @@ public:
         }
     }
 
-    Status sendFile(ServerContext *context, const empty* request, file_from_server *ret) override {
+    Status initialize(ServerContext *context, const empty* request, file_from_server *ret) override {
         for (std::string &s : content) {
             ret->add_file(s);
         }
+        ret->set_op_id(op_quantity);
         return Status::OK;
     }
 
-    Status writeToFile(ServerContext* context, const empty* request, empty* write) override {
+    Status writeToFile(ServerContext* context, const empty* request, last_executed_operation* write) override {
         std::ofstream f(file_name);
         for(std::string& s : content) {
             f << s << std::endl;
         }
+        write->set_op_id(op_quantity);
         return Status::OK;
     }
 
-    Status sendOP(ServerContext* context, const editor_request* OP, empty* ret) override {
-        OP_type op_type = OP->op();
-        uint64_t pos = OP->pos();
-        uint64_t line = OP->line();
-        char sym = OP->sym();
-        uint32_t user_id = OP->user_id();
+    Status sendOP(ServerContext* context, const editor_request* OP, last_executed_operation* ret) override {
+        editor_request op = *OP;
+        op_vector.OPT(op);
+        OP_type op_type = op.op();
+        uint64_t pos = op.pos();
+        uint64_t line = op.line();
+        char sym = op.sym();
+        uint32_t user_id = op.user_id();
+        uint64_t last_user_op_id = op.op_id();
         switch (op_type) {
             case operationTransportation::INSERT: {
-                op_vector.add(*OP);
+                op_vector.add(op);
                 insert(content, sym, pos, line);
                 break;
             }
             case operationTransportation::DELETE: {
-                auto a = *OP;
-                a.set_sym(content[line][pos]);
-                op_vector.add(a);
+                op.set_sym(content[line][pos]);
+                op_vector.add(op);
                 del(content, content[line][pos], pos, line);
                 break;
             }
@@ -254,17 +303,20 @@ public:
                 break;
             }
             case operationTransportation::ADD_LINE: {
-                op_vector.add(*OP);
+                op_vector.add(op);
                 add_line(content, pos,line);
                 break;
             }
             case operationTransportation::DEL_LINE: {
-                op_vector.add(*OP);
+                op_vector.add(op);
                 del_line(content,line);
+                break;
             }
             default:
                 return Status::CANCELLED;
         }
+        ++op_quantity;
+        ret->set_op_id(op_quantity);
         return Status::OK;
     }
 };
